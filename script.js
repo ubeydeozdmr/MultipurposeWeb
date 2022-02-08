53388feda93d0e4c26630c926d70807bb63091e0
Ubeyde Emir Özdemir <ubeydeozdmr@gmail.com>
2022-02-08 16:44:11 +0300

    Project has been rebuilt

--- script.js
+++ script.js
@@ -1,33 +1,112 @@
-'use strict';
+// ! ELEMENTS
 
-// NUMBER GUESSING GAME
-const btnSendGuessedNumber = document.querySelector(
-  '#collapseTwo .input-group button'
-);
-const btnResetGuessingGame = document.querySelector('#resetGame');
+// ? Number Guessing Game
+const inputGuessingGame = document.getElementById('input-num-ngg');
+const btnSendGuessedNumber = document.getElementById('btn-send-ngg');
+const btnResetGuessingGame = document.getElementById('btn-reset-ngg');
+const resultNumberGuessing = document.getElementById('result-ngg');
+const labelLives = document.getElementById('p-lives-ngg');
+
+// ? Prime Number Finder
+const btnSendPrimeNumber = document.getElementById('btn-send-pnf');
+const inputPrimeNumber = document.getElementById('input-num-pnf');
+const resultPrimeNumber = document.getElementById('result-pnf');
+
+// ? Simple Calculator
+const calcAction = document.getElementById('calc-action-sc');
+const firstNumber = document.getElementById('first-number-sc');
+const secondNumber = document.getElementById('second-number-sc');
+const btnAddition = document.getElementById('btn-a-sc');
+const btnSubtraction = document.getElementById('btn-s-sc');
+const btnMultiplication = document.getElementById('btn-m-sc');
+const btnDivision = document.getElementById('btn-d-sc');
+const btnCalculate = document.getElementById('btn-send-sc');
+const resultSimpleCalculator = document.getElementById('result-sc');
+
+// ? Text Repeater
+const inputRepeatNum = document.getElementById('input-num-tr');
+const inputRepeatText = document.getElementById('input-text-tr');
+const flexCheckSpace = document.getElementById('flex-check-space-tr');
+const flexCheckLine = document.getElementById('flex-check-line-tr');
+const resultTextRepeater = document.getElementById('result-tr');
+const btnRepeat = document.getElementById('btn-send-tr');
+const textarea = document.getElementById('textarea-tr');
+const btnCopy = document.getElementById('btn-copy-tr');
+const btnClear = document.getElementById('btn-clear-tr');
+
+// ? Converter
+const inputTemperature = document.getElementById('input-temperature-c');
+const inputTmpDisable = document.getElementById('input-temperature-disabled-c');
+const btnSendTemp = document.getElementById('btn-send-c');
+const flexRadioCtoF = document.getElementById('flex-radio-cf-c');
+const flexRadioFtoC = document.getElementById('flex-radio-fc-c');
+const resultTemperature = document.getElementById('result-c');
+const btnReverse = document.getElementById('btn-temp-reverse-c');
+
+// ! GLOBAL VARIABLES
+
+// ? Number Guessing Game
 let randomNumber;
 let lives;
+let getGuessedNumber;
+let compr;
 
-btnSendGuessedNumber.addEventListener('click', function () {
-  console.log(randomNumber);
-  let getGuessedNumber = document.querySelector('#collapseTwo input').value;
-  if (getGuessedNumber != randomNumber) {
-    let i;
-    getGuessedNumber < randomNumber ? (i = 'bigger') : (i = 'smaller');
-    document.querySelector(
-      '#collapseTwo .p'
-    ).textContent = `Try entering a ${i} number.`;
+// ? Prime Number Finder
+let getPrimeNumber;
+let controller;
+let inputtedNumber1;
+let inputtedNumber2;
+let resultOp;
+
+// ? Converter
+let tempController;
+
+// ! FUNCTIONS
+
+// ? Number Guessing Game
+
+const guessAgain = () => {
+  lives = 7;
+  randomNumber = Math.trunc(Math.random() * 100);
+  labelLives.textContent = 'Lives: ' + lives;
+  inputGuessingGame.value = '';
+  inputGuessingGame.removeAttribute('disabled');
+  resultNumberGuessing.textContent = '';
+};
+
+// ? Prime Number Finder
+
+const isEven = num => num % 2 === 0;
+
+// ? Text Repeater
+
+const repeater = (space, line) => {
+  for (let i = 0; i < inputRepeatNum.value; i++)
+    textarea.innerHTML += inputRepeatText.value + space + line;
+};
+
+const copyToClipboard = () => navigator.clipboard.writeText(textarea.value);
+
+// ! HANDLERS
+
+// ? Number Guessing Game
+btnSendGuessedNumber.addEventListener('click', () => {
+  getGuessedNumber = Number(inputGuessingGame.value);
+  if (getGuessedNumber !== randomNumber) {
+    getGuessedNumber < randomNumber ? (compr = 'bigger') : (compr = 'smaller');
+    resultNumberGuessing.textContent = `Try entering a ${compr} number.`;
     lives--;
-    document.querySelector('#lives').textContent = 'Lives: ' + lives;
+    labelLives.textContent = 'Lives: ' + lives;
   } else {
-    document.querySelector('#collapseTwo .p').textContent =
-      'You guessed right, excellent! If you want to play again, try entering a new number.';
-    document.querySelector('#collapseTwo input');
+    inputGuessingGame.setAttribute('disabled', true);
+    resultNumberGuessing.textContent =
+      'You guessed right, excellent! If you want to play again, try entering a new number';
   }
 
   if (lives === 0) {
+    inputGuessingGame.setAttribute('disabled', true);
     alert(
-      "sorry, you didn't guess correctly. The number you had to guess was: " +
+      "Sorry, you didn't guess correctly. The number you had to guess was: " +
         randomNumber
     );
     guessAgain();
@@ -36,159 +115,92 @@ btnSendGuessedNumber.addEventListener('click', function () {
 
 btnResetGuessingGame.addEventListener('click', guessAgain);
 
-function guessAgain() {
-  lives = 7;
-  randomNumber = Math.trunc(Math.random() * 100);
-  document.querySelector('#lives').textContent = 'Lives: ' + lives;
-  document.querySelector('#collapseTwo input').value = '';
-}
+// ? Prime Number Finder
+btnSendPrimeNumber.addEventListener('click', () => {
+  getPrimeNumber = Number(inputPrimeNumber.value);
+  controller = false;
 
-// PRIME NUMBER FINDER
-const btnSendPrimeNumber = document.querySelector('#collapseThree button');
-btnSendPrimeNumber.addEventListener('click', function () {
-  let getPrimeNumber = document.querySelector('#collapseThree input').value;
-  let controller = 0;
-
-  for (let i = 2; i < getPrimeNumber; i++) {
-    if (getPrimeNumber % i == 0) {
-      controller++;
-      break;
+  if (getPrimeNumber <= 0)
+    resultPrimeNumber.textContent = `The number you enter must be a positive integer. Please enter an integer greater than zero.`;
+  else if (getPrimeNumber === 2)
+    resultPrimeNumber.textContent = `The number ${getPrimeNumber} you entered is a prime number.`;
+  else if (getPrimeNumber === 1 || isEven(getPrimeNumber))
+    resultPrimeNumber.textContent = `The number ${getPrimeNumber} you entered is not a prime number.`;
+  else {
+    for (let i = 2; i <= getPrimeNumber / 2; i++) {
+      if (getPrimeNumber % i === 0) {
+        controller = true;
+        break;
+      }
     }
-  }
-
-  if (getPrimeNumber <= 0) {
-    document.querySelector('#collapseThree p').textContent =
-      'The number you enter must be a positive integer. Please enter an integer greater than zero.';
-  } else if (controller != 0 || getPrimeNumber == 1) {
-    document.querySelector(
-      '#collapseThree p'
-    ).textContent = `The number ${getPrimeNumber} you entered is not a prime number.`;
-  } else {
-    document.querySelector(
-      '#collapseThree p'
-    ).textContent = `The number ${getPrimeNumber} you entered is a prime number.`;
+    controller === true
+      ? (resultPrimeNumber.textContent = `The number ${getPrimeNumber} you entered is not a prime number.`)
+      : (resultPrimeNumber.textContent = `The number ${getPrimeNumber} you entered is a prime number.`);
   }
 });
 
-// SIMPLE CALCULATOR
-const btnCalculate = document.querySelector('#calculate');
-
-const btnAddition = document.querySelector(
-  '#collapseFour ul li:nth-child(1) .dropdown-item'
-);
-const btnSubtraction = document.querySelector(
-  '#collapseFour ul li:nth-child(2) .dropdown-item'
-);
-const btnMultiplication = document.querySelector(
-  '#collapseFour ul li:nth-child(3) .dropdown-item'
-);
-const btnDivision = document.querySelector(
-  '#collapseFour ul li:nth-child(4) .dropdown-item'
+// ? Simple Calculator
+[btnAddition, btnSubtraction, btnMultiplication, btnDivision].forEach(
+  operation => {
+    operation.style.cursor = 'pointer';
+    operation.addEventListener(
+      'click',
+      () => (calcAction.textContent = operation.textContent)
+    );
+  }
 );
 
-for (let i = 0; i < 4; i++) {
-  document.querySelectorAll('#collapseFour .dropdown-item')[i].style.cursor =
-    'pointer';
-}
+btnCalculate.addEventListener('click', () => {
+  inputtedNumber1 = Number(firstNumber.value);
+  inputtedNumber2 = Number(secondNumber.value);
 
-btnAddition.addEventListener('click', function () {
-  document.querySelector('#calc-action').textContent = btnAddition.textContent;
-});
-btnSubtraction.addEventListener('click', function () {
-  document.querySelector('#calc-action').textContent =
-    btnSubtraction.textContent;
-});
-btnMultiplication.addEventListener('click', function () {
-  document.querySelector('#calc-action').textContent =
-    btnMultiplication.textContent;
-});
-btnDivision.addEventListener('click', function () {
-  document.querySelector('#calc-action').textContent = btnDivision.textContent;
-});
-
-btnCalculate.addEventListener('click', function () {
-  let inputtedNumber1 = Number(document.querySelector('#first-number').value);
-  let inputtedNumber2 = Number(document.querySelector('#second-number').value);
-  let result;
-
-  switch (document.querySelector('#calc-action').textContent) {
-    case 'Addition':
-      result = inputtedNumber1 + inputtedNumber2;
+  switch (calcAction.textContent) {
+    case '+':
+      resultOp = inputtedNumber1 + inputtedNumber2;
       break;
-    case 'Subtraction':
-      result = inputtedNumber1 - inputtedNumber2;
+    case '-':
+      resultOp = inputtedNumber1 - inputtedNumber2;
       break;
-    case 'Multiplication':
-      result = inputtedNumber1 * inputtedNumber2;
+    case '*':
+      resultOp = inputtedNumber1 * inputtedNumber2;
       break;
-    case 'Division':
-      result = inputtedNumber1 / inputtedNumber2;
+    case '/':
+      resultOp = inputtedNumber1 / inputtedNumber2;
+      break;
+    default:
+      resultOp = 'Please select an action';
       break;
   }
-
-  document.querySelector('#collapseFour p').textContent = 'Result: ' + result;
+  resultSimpleCalculator.textContent = 'Result: ' + resultOp;
 });
 
-// TEXT REPEATER
-const inputRepeatNum = document.querySelector(
-  '#collapseFive .input-group:nth-child(1) input'
+// ? Text Repeater
+
+btnRepeat.addEventListener('click', () =>
+  flexCheckLine.checked
+    ? flexCheckSpace.checked
+      ? repeater(' ', '\n')
+      : repeater('', '\n')
+    : flexCheckSpace.checked
+    ? repeater(' ', '')
+    : repeater('', '')
 );
-const inputRepeatText = document.querySelector(
-  '#collapseFive .input-group:nth-child(2) input'
-);
-const btnRepeat = document.querySelector('#collapseFive button');
-btnRepeat.addEventListener('click', function () {
-  if (document.querySelector('#collapseFive #flexCheckLine').checked) {
-    document.querySelector('#collapseFive #flexCheckSpace').checked
-      ? RepeaterFunction(
-          ' ',
-          `
-`
-        )
-      : RepeaterFunction(
-          '',
-          `
-`
-        );
-  } else {
-    document.querySelector('#collapseFive #flexCheckSpace').checked
-      ? RepeaterFunction(' ', '')
-      : RepeaterFunction('', '');
-  }
-});
-const btnClear = document.querySelector('#clear');
-btnClear.addEventListener('click', function () {
-  document.querySelector('#collapseFive textarea').innerHTML = null;
-});
-const btnCopy = document.querySelector('#copy');
-btnCopy.addEventListener('click', function () {
-  let copyText = document.querySelector('#collapseFive textarea');
-  navigator.clipboard.writeText(copyText.value);
-});
-const RepeaterFunction = function (space, enter) {
-  for (let i = 0; i < inputRepeatNum.value; i++) {
-    document.querySelector('#collapseFive textarea').innerHTML +=
-      inputRepeatText.value + space + enter;
-  }
-};
 
-// TEMPERATURE CONVERTER
-const tempInput = document.querySelector('#tempinput');
-const buttonSendTemp = document.querySelector('#button-send-temp');
-const flexRadioCtoF = document.querySelector('#flexRadioCtoF');
-const flexRadioFtoC = document.querySelector('#flexRadioFtoC');
-let resultTemp = document.querySelector('#collapseSix p');
-buttonSendTemp.addEventListener('click', function () {
-  let temp = tempInput.value;
-  if (document.querySelector('#flexRadioCtoF').checked) {
-    resultTemp.textContent = 'Result: ' + (temp * 1.8 + 32);
-  } else if (document.querySelector('#flexRadioFtoC').checked) {
-    resultTemp.textContent = 'Result: ' + (temp - 32) / 1.8;
-  }
+btnClear.addEventListener('click', () => (textarea.innerHTML = null));
+btnCopy.addEventListener('click', copyToClipboard);
+
+// ? Converter
+tempController = false;
+btnSendTemp.addEventListener('click', () => {
+  tempController
+    ? (inputTmpDisable.value = (inputTemperature.value - 32) / 1.8)
+    : (inputTmpDisable.value = inputTemperature.value * 1.8 + 32);
 });
-flexRadioCtoF.addEventListener('click', function () {
-  tempInput.placeholder = 'Please enter the temperature in celcius';
-});
-flexRadioFtoC.addEventListener('click', function () {
-  tempInput.placeholder = 'Please enter the temperature in fahrenheit';
+
+btnReverse.addEventListener('click', () => {
+  tempController === false ? (tempController = true) : (tempController = false);
+  [inputTemperature.value, inputTmpDisable.value] = [
+    inputTmpDisable.value,
+    inputTemperature.value,
+  ];
 });
